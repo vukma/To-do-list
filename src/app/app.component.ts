7f263af6444e6ca72ee1e2cf5049b6996200e268
vukma <vukmirovmiroslav@gmail.com>
2024-05-25 09:23:05 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  todos : Todo[] = []
+  newTodo : string;
+  //title = 'todo';
+
+  saveTodo() {
+    if(this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true
+      this.todos.push(todo);
+      this.newTodo = '';
+    }else {
+      alert('Please Enter Todo')
+    }
+  }
+
+
+  done (id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter((v,i)=> i !== id);
+  }
 }
